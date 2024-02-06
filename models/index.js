7277ba827a2912023742f69209abef89f9054e60
jakeshmith <153260507+jakeshmith@users.noreply.github.com>
2024-02-05 19:50:50 -0600

    finished index file

--- models/index.js
+++ models/index.js
@@ -5,13 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+}),
 // Categories have many Products
-
+Category.HasMany(Product, {
+  foreignKey: "category_id",
+}),
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+}),
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+}),
 
+// Exports
 module.exports = {
   Product,
   Category,
